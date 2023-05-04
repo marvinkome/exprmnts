033330260f96f42beeab697dba91089447baead1
Marvin Kome <marvin.kome@movinga.com>
2023-05-05 00:13:05 +0200

    add memo homepage animations

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,16 @@
 import "./globals.css";
-import { Figtree } from "@next/font/google";
+import { Figtree, Fraunces } from "@next/font/google";
 
+const fraunces = Fraunces({
+  subsets: ["latin"],
+  axes: ["SOFT", "opsz"],
+  variable: "--font-fraunces",
+});
 const figtree = Figtree({ subsets: ["latin"], variable: "--font-figtree" });
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en" className={`${figtree.variable} h-full`}>
+    <html lang="en" className={`${figtree.variable} ${fraunces.variable} h-full`}>
       <head />
       <body className={`h-full`}>{children}</body>
     </html>
